d312d3ffa67512bc6ef1723194e4eec84401468a
Andrea Massignan <andreamassignan98@gmail.com>
2024-04-18 11:10:02 +0200

    Now it does something, still not finished

--- lab5/es2/src/turtle_patrol/turtle_patrol/client.py
+++ lab5/es2/src/turtle_patrol/turtle_patrol/client.py
@@ -1,45 +1,32 @@
+# Save this as patrolling_client.py
 import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 from action_tutorials_interfaces.action import Patrolling
 
-class PatrollingActionClient(Node):
-    
+class PatrollingClient(Node):
+
     def __init__(self):
-        super().__init__('patrolling_action_client')
+        super().__init__('patrolling_client')
         self._action_client = ActionClient(self, Patrolling, 'patrolling')
-    
-    def send_goal(self, points):
+        self._send_goal()
+
+    def _send_goal(self):
         goal_msg = Patrolling.Goal()
-        goal_msg.points = points
-        
+        # Define your sequence of poses here, e.g., [(x1, y1), (x2, y2), ...]
+        goal_msg.goal = [5.0, 5.0]
         self._action_client.wait_for_server()
-        self._action_client.send_goal(goal_msg, feedback_callback=self.feedback_callback)
-    
+        self.get_logger().info('Sending patrolling goal...')
+        self._action_client.send_goal(goal_msg)
+
     def feedback_callback(self, feedback_msg):
-        self.get_logger().info('Received feedback: {0}'.format(feedback_msg.current_position))
-    
-    def get_result(self):
-        self._action_client.wait_for_result()
-        return self._action_client.get_result()
+        self.get_logger().info('Feedback received: {}'.format(feedback_msg.feedback[0]))
 
-
-def main():
-    rclpy.init()
-
-
-    patrolling_action_client = PatrollingActionClient()
-    points = [1, 2]
-    patrolling_action_client.send_goal(points)
-
-    result = patrolling_action_client.get_result()
-    if result:
-        patrolling_action_client.get_logger().info('Final position: {0}'.format(result.final_position))
-    else:
-        patrolling_action_client.get_logger().info('Action failed')
-
+def main(args=None):
+    rclpy.init(args=args)
+    patrolling_client = PatrollingClient()
+    rclpy.spin(patrolling_client)
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
--- lab5/es2/src/turtle_patrol/turtle_patrol/server.py
+++ lab5/es2/src/turtle_patrol/turtle_patrol/server.py
@@ -1,61 +1,93 @@
 import rclpy
 from rclpy.action import ActionServer
 from rclpy.node import Node
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist, Pose as PoseMsg
 from action_tutorials_interfaces.action import Patrolling
-import time
+import math
 from rclpy.executors import MultiThreadedExecutor
-from geometry_msgs.msg import Twist
 
+class PatrollingServer(Node):
 
-class PatrollingActionServer(Node):
-    
     def __init__(self):
-        super().__init__('patrolling_action_server')
+        super().__init__('patrolling_server')
         self._action_server = ActionServer(
             self,
             Patrolling,
             'patrolling',
-            self.execute_callback
-        )
-        self._turtlesim_publisher = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
-    
+            self.execute_callback)
+        self._pose_subscriber = self.create_subscription(
+            Pose,
+            'turtle1/pose',
+            self.pose_callback,
+            10)
+        self._current_pose = Pose()
+        self._twist_publisher = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
+
     def execute_callback(self, goal_handle):
-        self.get_logger().info('Executing goal...')
+        self.get_logger().info('Executing patrolling action...')
+        goal = goal_handle.request.goal
+        #feedback_msg = Patrolling.Feedback()
         
-        feedback_msg = Patrolling.Feedback()
-        feedback_msg.current_position = [0, 0]
-        
-        for i in range(1, goal_handle.request.points):
-            feedback_msg.current_position = goal_handle.request.points[i]
-            self.get_logger().info('Feedback: {0}'.format(feedback_msg.current_position))
-            goal_handle.publish_feedback(feedback_msg)
-            
-            # Publish twist message to control the turtle's movement
-            twist_msg = Twist()
-            twist_msg.linear.x = 1.0  # Set linear velocity
-            twist_msg.angular.z = 0.5  # Set angular velocity
-            self._turtlesim_publisher.publish(twist_msg)
-            
-            time.sleep(1)
-        
-        # Stop the turtle's movement
-        twist_msg = Twist()
-        twist_msg.linear.x = 0.0
-        twist_msg.angular.z = 0.0
-        self._turtlesim_publisher.publish(twist_msg)
-        
-        goal_handle.succeed()
-        
+        self.move_to_pose(goal)
+        #feedback_pose = PoseMsg()
+        #feedback_pose.position.x = self._current_pose.x
+        #feedback_pose.position.y = self._current_pose.y
+        #feedback_msg.feedback.append(feedback_pose)
+        #goal_handle.publish_feedback(feedback_msg)
+        while not self.reached_goal(goal):
+            rclpy.spin_once(self)
         result = Patrolling.Result()
-        result.final_position = feedback_msg.current_position
-        return result
+        result.result = goal
+        goal_handle.succeed(result)
 
+    def pose_callback(self, msg):
+        self._current_pose = msg
 
-def main():
-    rclpy.init()
+    def move_to_pose(self, pose):
+        #Log pose
+        self.get_logger().info('Moving to pose: {}'.format(pose))
+        angle = math.atan2(pose[1] - self._current_pose.y, pose[0] - self._current_pose.x)
+        distance = math.sqrt((pose[0] - self._current_pose.x)**2 + (pose[1] - self._current_pose.y)**2)
+        self.rotate(angle)
+        self.move(distance)
 
+    def rotate(self, angle):
+        angular_speed = 0.5
+        linear_speed = 0.0
+        twist = Twist()
+        twist.linear.x = linear_speed
+        twist.angular.z = angular_speed
+        self._twist_publisher.publish(twist)
+        time = abs(angle / angular_speed)
+        self.get_logger().info('Rotating for {} seconds'.format(time))
+        self.create_timer(time, self.stop_moving)
+    
+    def stop_moving(self):
+        twist = Twist()
+        self._twist_publisher.publish(twist)
+        
+    def move(self, distance):
+        angular_speed = 0.0
+        linear_speed = 0.5
+        twist = Twist()
+        twist.linear.x = linear_speed
+        twist.angular.z = angular_speed
+        self._twist_publisher.publish(twist)
+        time = distance / linear_speed
+        self.get_logger().info('Moving for {} seconds'.format(time))
+        self.create_timer(time, self.stop_moving)
+    
+    def reached_goal(self, pose):
+        distance = math.sqrt((pose[0] - self._current_pose.x)**2 + (pose[1] - self._current_pose.y)**2)
+        return distance < 0.1
+    
+def main(args=None):
+    rclpy.init(args=args)
     executor = MultiThreadedExecutor()
-    patrolling_action_server = PatrollingActionServer()
-    executor.add_node(patrolling_action_server)
-
+    patrolling_server = PatrollingServer()
+    executor.add_node(patrolling_server)
     executor.spin()
+
+if __name__ == '__main__':
+    main()
